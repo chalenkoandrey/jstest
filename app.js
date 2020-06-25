e2ded174696d5c6d6da1a33ec8a8ff26ce58e255
Andrew Chalenko <andrysha1999@gmail.com>
2020-06-25 12:45:23 +0300

    simple server

--- app.js
+++ app.js
@@ -1,53 +1,80 @@
 const express = require("express");
-const fs = require("fs");
-const fetch = require("node-fetch");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const router = express.Router();
+var bodyParser = require('body-parser')
 
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
 
-const string = "abc";
-const url = "http://nodejs.org/dist/index.json";
-const fileRead = "1.txt"
-const type = "UTF-8"
-const fileWrite = "rez.txt"
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(router)
 
+router.route("/users/count")
+  .get((req, res) => {
+    countUsers()
+      .then(rez => {
+        res.json({ "count": rez })
+          .status(200)
+          .send()
+      })
+      .catch(err => {
+        res.json(err)
+          .status(500)
+          .send()
+      })
 
-function readFile(fileName, type) {
+  })
+
+router.route("/users")
+  .get((req, res) => {
+    findUsers(req.body.skip, req.body.limit)
+      .then(rez => {
+        res.json(rez)
+          .status(200)
+          .send()
+      })
+      .catch(err => {
+        res.json(err)
+          .status(500)
+          .send()
+      })
+  })
+
+function findUsers(start, limit) {
   return new Promise((resolve, reject) => {
-    fs.readFile(fileName, type, (err, data) => {
-      err ? reject(err) : resolve(data);
-    });
-  });
-}
-
-function writeToFile(fileName, data) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(fileName, data, err => {
-      err ? reject(err) : resolve();
+    mongoClient.connect((err, client) => {
+      if (err) {
+        return reject(err);
+      }
+      else {
+        const db = client.db("UserDB")
+        const collection = db.collection("users");
+        collection.find({}).toArray(function (err, result) {
+          err ? reject(err) : resolve(result.splice(start, limit));
+        })
+      }
     });
   })
 }
 
-function getDataByUrl(url) {
-  return fetch(url)
-    .then(response => response.json())
-    .then(rez => JSON.stringify(rez))
-}
-
-function test(fileRead, type) {
-  return Promise.all([readFile(fileRead, type), getDataByUrl(url)])
+function countUsers() {
+  return new Promise((resolve, reject) => {
+    mongoClient.connect((err, client) => {
+      if (err) {
+        return resolve(err);
+      }
+      else {
+        const db = client.db("UserDB")
+        const collection = db.collection("users");
+        collection.find({}).toArray(function (err, result) {
+          err ? reject(err) : resolve(result.length);
+        })
+      }
+    });
+  })
 }
 
 app.listen(9000, () => {
-  test(fileRead, type)
-    .then((rez) => {
-      console.log(rez);
-      return writeToFile(fileWrite, string + rez)
-    })
-    .then(() => {
-      console.log("Data has been saved")
-    })
-    .catch(e => {
-      console.log(e);
-    })
   console.log("Server is running on port 9000");
 });
